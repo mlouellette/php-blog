f79f15353e25d5f718e07b40a1f11a78a5892f12
mlouellette <michaelouellette27@hotmail.com>
2023-07-17 17:02:55 -0400

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./resources/views/layouts/app.blade.php",
+    "./resources/views/posts/index.blade.php",
+    "./resources/js/app.js",
+    "./resources/css/app.css"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
